e91e63208a96cd8785e545e26a408d882ccf7d4d
Kuroiwa Nobuhiro <Kuroiwa Nobuhiro>
2016-07-18 17:27:57 +0900

    notify when 2 same note detected

--- src/HelloIot/EdgeDetector.cpp
+++ src/HelloIot/EdgeDetector.cpp
@@ -1,10 +1,9 @@
 #include "EdgeDetector.h"
 
 
-EdgeDetector::EdgeDetector(int val) : _val(val)
+EdgeDetector::EdgeDetector(int val) : _history({0}), _lastNotifiedVal(0)
 {
 }
-
 
 EdgeDetector::~EdgeDetector()
 {
@@ -12,7 +11,16 @@ EdgeDetector::~EdgeDetector()
 
 bool EdgeDetector::Input(uint16_t val)
 {
-	uint16_t _oldVal = _val;
-	_val = val;
-	return (_oldVal != _val);
+	_history[0] = _history[1];
+	_history[1] = _history[2];
+	_history[2] = val;
+	if(_history[1] == _history[2]) {
+	    if(_history[0] != _history[1]) {
+	        if(_history[1] != _lastNotifiedVal) {
+	            _lastNotifiedVal = val;
+	            return true;
+	        }
+	    }
+	}
+	return false;
 }
